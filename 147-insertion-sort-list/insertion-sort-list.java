b5f241d2b26fcc97e9664231c8905be02b1468aa
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-12 15:18:22 +0530

    Time: 20 ms (21.39%) | Memory: 44.5 MB (56.92%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -12,16 +12,17 @@
 
  class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ListNode dummy=new ListNode(-1);
-        ListNode temp=head;
-        while(temp!=null){
-            ListNode agla=temp.next;
-            ListNode pichla=dummy;
-            while(pichla.next!=null && pichla.next.val<=temp.val)
-                pichla=pichla.next;
-            temp.next=pichla.next;
-            pichla.next=temp;
-            temp=agla;
+        ListNode dummy= new ListNode(0);
+        ListNode current=head;
+        while(current != null){
+            ListNode prev=dummy;
+            ListNode nextNode=current.next;
+            while(prev.next!=null && prev.next.val < current.val){
+                prev=prev.next;
+            }
+            current.next=prev.next;
+            prev.next=current;
+            current=nextNode;
         }
         return dummy.next;
     }
