e713ef38e61ac486cab8989a61e2df796e6b95a8
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-13 22:55:42 +0530

    Time: 18 ms (45.25%) | Memory: 44.2 MB (43.01%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        List<Long> path = new ArrayList<>();
+        return helper(root, targetSum, path);
+    }
+
+    int helper(TreeNode node, int targetSum, List<Long> path) {
+        if(node == null) return 0;
+
+        path.add((long) node.val);
+        long sum = 0;
+        int count = 0;
+
+        ListIterator<Long> itr = path.listIterator(path.size());
+        while(itr.hasPrevious()) {
+            sum += itr.previous();
+            if(sum == targetSum) count++;
+        }
+
+        count += helper(node.left, targetSum, path);
+        count += helper(node.right, targetSum, path);
+
+         path.remove(path.size() - 1); // Backtrack
+
+        return count;
+    }
+}
