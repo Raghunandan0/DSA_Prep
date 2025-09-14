95de2bdf80cc59773ff2ea0dc38ed0ce9dd2b8e0
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-14 10:27:54 +0530

    Time: 4 ms (64.73%) | Memory: 55.8 MB (91.74%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,14 +1,27 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int windowsum = 0;
-        for(int i=0; i<k; i++) {
-            windowsum+=nums[i];
+        double windowSum = 0;
+        double maxSum = 0;
+
+        // Check for edge case
+        if (nums.length < k) {
+            System.out.println("Invalid: Window size is larger than array.");
+            return -1;
         }
-        int max = windowsum;
-        for(int i=k; i<nums.length; i++) {
-            windowsum += nums[i] - nums[i-k];
-            max = Math.max(max, windowsum);
+
+        // Calculate the sum of the first window
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
         }
-        return(double) max/k;
+
+        maxSum = windowSum;
+
+        // Slide the window
+        for (int i = k; i < nums.length; i++) {
+            windowSum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum/k;
     }
 }
