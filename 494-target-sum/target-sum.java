0f18400bc367d401f91c492daf36cda0d3e13b2d
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-18 23:33:59 +0530

    Time: 5 ms (83.34%) | Memory: 43.8 MB (45.27%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,29 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
-    }
+        int total = 0;
+        for (int num : nums) total += num;
 
-    private int helper(int[] arr, int target, int idx, int sum) {
-        if (idx >= arr.length)
-            return sum == target ? 1 : 0;
-        
-        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
-        int add = helper(arr, target, idx + 1, sum + arr[idx]);
+        // Edge case: invalid target
+        if ((target + total) % 2 != 0 || target > total || target < -total) {
+            return 0;
+        }
 
-        return subtract + add;
+        int sum = (target + total) / 2;
+        int n = nums.length;
+
+        // dp[i][j] = number of ways to form sum j using first i elements
+        int[][] dp = new int[n + 1][sum + 1];
+        dp[0][0] = 1; // 1 way to form sum 0 with 0 elements
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                dp[i][j] = dp[i - 1][j]; // exclude nums[i-1]
+                if (nums[i - 1] <= j) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]]; // include nums[i-1]
+                }
+            }
+        }
+
+        return dp[n][sum];
     }
 }
