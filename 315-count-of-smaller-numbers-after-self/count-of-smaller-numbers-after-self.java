587a278135c2cd43712c1c0ca0bddf20e363c295
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-19 11:27:54 +0530

    Time: 1198 ms (8.85%) | Memory: 65.6 MB (13.81%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,28 +1,31 @@
+
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        List<Integer> li = new ArrayList<>();
-        List<Integer> sorted = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        List<Integer> sortedList = new ArrayList<>();
 
-        for(int i=nums.length-1;i>=0;i--){
-            int ind = insert(sorted,nums[i]);
-            li.add(ind);
-            sorted.add(ind, nums[i]);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int num = nums[i];
+            int idx = bisectLeft(sortedList, num);
+            result.add(idx);
+            sortedList.add(idx, num);
         }
 
-        Collections.reverse(li);
-        return li;
+        Collections.reverse(result);  // The result needs to be reversed
+        return result;
     }
-    private int insert(List<Integer> arr, int num){
-        int l =0;
-        int r = arr.size()-1;
 
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(arr.get(mid)< num){
-                l = mid+1;
-            }else r = mid-1;
+    private int bisectLeft(List<Integer> container, int insertVal) {
+        int l = 0;
+        int r = container.size()-1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (container.get(mid) < insertVal) {
+                l = mid + 1;
+            } else {
+                r = mid-1;
+            }
         }
-
         return l;
     }
 }
