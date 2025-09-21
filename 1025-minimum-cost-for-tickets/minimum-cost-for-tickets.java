4dd86aaa9a138c1785e78051738be0a068893784
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-21 23:35:14 +0530

    Time: 1 ms (75.34%) | Memory: 41.5 MB (77.13%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,23 +1,25 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        int n = days.length;
-        int lastDay = days[n-1];
-        int[] dp = new int[lastDay + 1]; // dp[day] = min cost up to this day
-        boolean travelDay[] = new boolean[lastDay + 1];
+        int maxDay = days[days.length - 1];
+        boolean[] travelDay = new boolean[maxDay + 1];
 
-        for (int d : days) travelDay[d] = true;
+        for (int day : days)
+            travelDay[day] = true;
 
-        for (int d = 1; d <= lastDay; d++) {
-            if (!travelDay[d]) {
-                dp[d] = dp[d-1]; // no travel today, cost same as yesterday
-            } else {
-                int cost1 = dp[d-1] + costs[0];                 // 1-day pass
-                int cost7 = dp[Math.max(0, d-7)] + costs[1];    // 7-day pass
-                int cost30 = dp[Math.max(0, d-30)] + costs[2];  // 30-day pass
-                dp[d] = Math.min(cost1, Math.min(cost7, cost30));
+        int[] dp = new int[maxDay + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= maxDay; i++) {
+            if (!travelDay[i]) {
+                dp[i] = dp[i - 1];
+                continue;
             }
+
+            dp[i] = costs[0] + dp[i - 1];
+            dp[i] = Math.min(dp[Math.max(0, i - 7)] + costs[1], dp[i]);
+            dp[i] = Math.min(dp[Math.max(0, i - 30)] + costs[2], dp[i]);
         }
 
-        return dp[lastDay];
+        return dp[maxDay];
     }
 }
