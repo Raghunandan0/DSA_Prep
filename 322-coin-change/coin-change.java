a24e203baa519ca5829bdfcf94cdeefc57da85df
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-21 23:31:25 +0530

    Time: 24 ms (33.64%) | Memory: 44.5 MB (66.15%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,18 +1,40 @@
 class Solution {
 
+    int[] dp;
     public int coinChange(int[] coins, int amount) {
 
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++) {
-            for(int j = 0; j < coins.length; j++) {
+        dp = new int[amount + 1];
+        Arrays.fill(dp, -1);
+        int ans = coinCount(coins, amount);
+        return (ans == Integer.MAX_VALUE) ?  -1 : ans;
+    }
 
-                if(i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
-                }
+    int coinCount(int[] coins, int amount) {
+
+        if(amount == 0) {
+            return 0;
+        }
+        if(amount < 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        if(dp[amount] != -1) {
+            return dp[amount];
+        }
+
+        int minCoins = Integer.MAX_VALUE;
+        for(int i = 0; i < coins.length; i++) {
+            int ans = coinCount(coins, amount - coins[i]);
+
+            if(ans != Integer.MAX_VALUE) {
+
+                //we have returned 0 in ans, so now we are updating the ans count
+                //hence 1 + ans
+                minCoins = Math.min(minCoins, 1 + ans);
             }
         }
-        return (dp[amount] == Integer.MAX_VALUE) ?  -1 : dp[amount];
+        return dp[amount] = minCoins;
     }
-}    
+}
+
+
