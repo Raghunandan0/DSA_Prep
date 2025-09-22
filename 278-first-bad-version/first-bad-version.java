b2b1212dc6906c3b4765a9a1a1870f0d963b92ad
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-22 09:16:06 +0530

    Time: 50 ms (5.01%) | Memory: 40 MB (97.49%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -3,26 +3,21 @@
 
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
+
     public int firstBadVersion(int n) {
-        int ans = -1;
-        int start = 1;
-        int prev = -1;
-        while (start <= n) {
-            int mid = start + (n - start) / 2;
-            if (isBadVersion(mid)) {
-                prev = mid;
-                n = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-            if (isBadVersion(mid)) {
-                ans = mid;
-            } else {
-                ans = prev;
-            }
-        }
-        return ans;
+        int low = 0;
+        int high = n;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)==true && isBadVersion(mid-1)==false)   return mid;
+            else if(isBadVersion(mid)==false ) low = mid+1;
+            else high= mid;
+        }     
+        return -1;
     }
 }
