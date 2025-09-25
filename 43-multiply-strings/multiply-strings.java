282598851871889a20c348eb3235678e196bbd5e
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-25 10:27:53 +0530

    Time: 2 ms (99.00%) | Memory: 42.4 MB (64.23%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,28 +1,27 @@
 class Solution {
-    public String multiply(String num1, String num2) {
-        if(num1.equals("0") || num2.equals("0")){
-            return "0";
-        }
-       
-        
-        int arr[] = new int [num1.length() + num2.length()];
-        for (int i=num1.length()-1;i>=0;i--){
-            for(int j=num2.length()-1;j>=0;j--){
-                int res = (num1.charAt(i)-'0')*(num2.charAt(j)-'0') + arr[i+j+1];
-                arr[i+j+1]=res%10;
-                arr[i+j] += res/10;
-            }
-        }
+	public String multiply(String num1, String num2) {
+		if ("0".equals(num1) || "0".equals(num2))
+			return "0";
 
-        StringBuilder sb = new StringBuilder();
-        
-        for(int k : arr){
-            if(sb.length()==0 && k==0){
-                continue;
-            }
-            sb.append(k);
-        }
-        return sb.toString();
+		int[] ans = new int[num1.length() + num2.length() - 1];
 
-    }
+		for (int i = 0; i < num1.length(); i++) {
+			for (int j = 0; j < num2.length(); j++) {
+				ans[i + j] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+			}
+		}
+
+		for (int i = ans.length - 1; i > 0; i--) {
+			ans[i - 1] += ans[i] / 10;
+			ans[i] %= 10;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i : ans) {
+			sb.append(i);
+		}
+
+		return sb.toString();
+	}
+
 }
