80896032e734de8418dd201f28e83a53602ca4c4
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-09-29 03:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (52.87%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low=1,high=num;
+        while(low<=high) {
+            int mid = (low + high) / 2;
+            if((long)mid* mid==num) return true;
+            else if((long)mid* mid >num) high = mid-1;
+            else low=mid+1;
+        }
+        return false;
+        
+    }
+}
