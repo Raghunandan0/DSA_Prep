eb4a316c4074e34a992ecd024e9190ed4db54494
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-10-04 12:26:55 +0530

    Time: 93 ms (82.09%) | Memory: 53 MB (80.38%) - LeetSync

--- 464-can-i-win/can-i-win.java
+++ 464-can-i-win/can-i-win.java
@@ -1,36 +1,60 @@
-import java.util.HashMap;
-import java.util.Map;
+/*
+     * some basic bitmask concept
+     * 
+     * 
+     * Set the ith bit: b(1<<i). Let i=0, so,
+     * (1<<i) = 00001
+     * 01010 | 00001 = 01011
+     * 
+     * Unset the ith bit: b & !(1<<i). Let i=1, so,
+     * (1<<i) = 00010
+     * !(1<<i) = 11101
+     * 01010 & 11101 = 01000
+     * 
+     * Let i = 3
+     * (1<<i) = 01000
+     * 01010 & 01000 = 01000
+     * 
+     * Clearly the result is non-zero, so that means 3rd element is present in the
+     * subset.
+     * 
+     * 
+     */
+
 
 class Solution {
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        if (desiredTotal == 0) {
-            return true;
-        }
-        if ((maxChoosableInteger * (maxChoosableInteger + 1)) / 2 < desiredTotal) {
-            return false;
-        }
-        
-        Map<Integer, Boolean> memo = new HashMap<>();
-        return canIWinHelper(maxChoosableInteger, desiredTotal, 0, memo);
+        return util1(maxChoosableInteger,desiredTotal);
     }
 
-    private boolean canIWinHelper(int maxChoosableInteger, int desiredTotal, int usedNumbers, Map<Integer, Boolean> memo) {
-        if (memo.containsKey(usedNumbers)) {
-            return memo.get(usedNumbers);
+    public boolean util1(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal) {
+            return false;
         }
-        
+        Boolean dp[] = new Boolean[1 << (maxChoosableInteger + 1)];
+        return helper(maxChoosableInteger, desiredTotal, dp, 0, 0);
+    }
+
+    public boolean helper(int maxChoosableInteger, int desiredTotal, Boolean dp[], int mask, int sum) {
+        if (dp[mask] != null) {
+            return dp[mask];
+        }
+        dp[mask] = false;
         for (int i = 1; i <= maxChoosableInteger; i++) {
-            int currentBit = 1 << i;
-            if ((usedNumbers & currentBit) == 0) { // Check if the number i is available
-                if (desiredTotal - i <= 0 || 
-                    !canIWinHelper(maxChoosableInteger, desiredTotal - i, usedNumbers | currentBit, memo)) {
-                    memo.put(usedNumbers, true);
-                    return true;
+            if ((mask & (1 << i)) == 0) {
+                if (sum + i >= desiredTotal) {
+                    dp[mask] = true;
+                } else {
+                    boolean hisAns = helper(maxChoosableInteger, desiredTotal, dp, (mask | (1 << i)), sum + i);
+                    if (hisAns == false) {
+                        dp[mask] = true;
+                    }
                 }
             }
+            if (dp[mask]) {
+                break;
+            }
         }
-        
-        memo.put(usedNumbers, false);
-        return false;
+        return dp[mask];
     }
 }
