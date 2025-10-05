df1ecf2b659a5fcc5704427c1b4c9fc3c5d1604b
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-10-05 23:36:04 +0530

    Time: 12 ms (78.27%) | Memory: 42.5 MB (58.01%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,28 +1,32 @@
+import java.util.*;
+
 class Solution {
-    int[][] dp;
-    int[] cuts;
-
     public int minCost(int n, int[] cuts) {
-        Arrays.sort(cuts);
-        this.cuts = cuts;
-        dp = new int[cuts.length][cuts.length];
-        return cost(0, cuts.length - 1, 0, n);
-    }
+        int m = cuts.length;
+        int[] newCuts = new int[m + 2];
+        newCuts[0] = 0;
+        newCuts[m + 1] = n;
+        
+        for (int i = 0; i < m; i++) {
+            newCuts[i + 1] = cuts[i];
+        }
+        Arrays.sort(newCuts);
 
-    private int cost(int i, int j, int start, int end) {
-        if (i > j) {
-            return 0; // no cuts to perform
+        int[][] dp = new int[m + 2][m + 2];
+
+        // bottom-up interval DP
+        for (int len = 2; len < m + 2; len++) {
+            for (int i = 0; i + len < m + 2; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i + 1; k < j; k++) {
+                    int cost = dp[i][k] + dp[k][j] + (newCuts[j] - newCuts[i]);
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
         }
-        if (dp[i][j] != 0) {
-            return dp[i][j]; // already computed
-        }
-        int minCost = Integer.MAX_VALUE;
-        for (int k = i; k <= j; k++) {
-            int fs = cost(i, k - 1, start, cuts[k]); // left stick
-            int ss = cost(k + 1, j, cuts[k], end);   // right stick
-            int self = end - start;                  // cost of current cut
-            minCost = Math.min(minCost, fs + ss + self);
-        }
-        return dp[i][j] = minCost;
+
+        return dp[0][m + 1];
     }
 }
