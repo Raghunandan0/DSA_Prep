c96030a84ff25a3f4d51f1889e0f0a20b6751d8f
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-10-29 00:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (38.57%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.java
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -1,32 +1,58 @@
+// class Solution {
+//     public int numberOfArithmeticSlices(int[] nums) {
+//         int count = 0;
+//         long sum = 0;
+
+//         for (int i = 1; i < nums.length - 1; i++) {
+//             if (nums[i] * 2 == nums[i - 1] + nums[i + 1]) {
+//                 if (count == 0)
+//                     count += 3; 
+//                 else
+//                     count += 1;
+//             } else {
+//                 if (count >= 3) { 
+//                     long totalSubArray = ((long) count * (long) (count + 1)) / 2;
+//                     totalSubArray -= count;         
+//                     totalSubArray -= (count - 1);  
+//                     sum += totalSubArray;
+//                 }
+//                 count = 0;
+//             }
+//         }
+
+//         if (count >= 3) {
+//             long totalSubArray = ((long) count * (long) (count + 1)) / 2;
+//             totalSubArray -= count;
+//             totalSubArray -= (count - 1);
+//             sum += totalSubArray;
+//         }
+
+//         return (int) sum;
+//     }
+// }
+
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        int count = 0;
-        long sum = 0;
+        if (nums.length < 3) return 0;  
+        
+        int sum = 0; 
+        int count = 0;   
+        int diff = nums[1] - nums[0];
 
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[i] * 2 == nums[i - 1] + nums[i + 1]) {
-                if (count == 0)
-                    count += 3; 
-                else
-                    count += 1;
+        for (int i = 2; i < nums.length; i++) {
+            int currDiff = nums[i] - nums[i - 1]; 
+
+            if (currDiff == diff) {
+                count++; 
             } else {
-                if (count >= 3) { 
-                    long totalSubArray = ((long) count * (long) (count + 1)) / 2;
-                    totalSubArray -= count;         
-                    totalSubArray -= (count - 1);  
-                    sum += totalSubArray;
-                }
-                count = 0;
+                sum += (count * (count + 1)) / 2; 
+                count = 0;  
+                diff = currDiff; 
             }
         }
 
-        if (count >= 3) {
-            long totalSubArray = ((long) count * (long) (count + 1)) / 2;
-            totalSubArray -= count;
-            totalSubArray -= (count - 1);
-            sum += totalSubArray;
-        }
+        sum += (count * (count + 1)) / 2;
 
-        return (int) sum;
+        return sum; 
     }
 }
