0d01b22d9325271d72f1536c598a9309718800ba
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-11-02 23:30:04 +0530

    Time: 76 ms (39.94%) | Memory: 59.3 MB (5.26%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,49 +1,51 @@
-import java.util.*;
-
 class Solution {
-   public int reversePairs(int[] nums) {
-       return mergeSort(nums, 0, nums.length - 1);
-   }
-   
-   private static void merge(int[] nums, int low, int mid, int high) {
-       ArrayList<Integer> temp = new ArrayList<>();
-       int left = low;
-       int right = mid + 1;
-       
-       while (left <= mid && right <= high) {
-           if (nums[left] <= nums[right]) {
-               temp.add(nums[left++]);
-           } else {
-               temp.add(nums[right++]);
-           }
-       }
-       while (left <= mid) temp.add(nums[left++]);
-       while (right <= high) temp.add(nums[right++]);
-       
-       for (int i = low; i <= high; i++) {
-           nums[i] = temp.get(i - low);
-       }
-   }
-   
-   private static int CountPairs(int[] nums, int low, int mid, int high) {
-       int cnt = 0;
-       int right = mid + 1;
-       for (int i = low; i <= mid; i++) {
-           while (right <= high && (long) nums[i] > 2L * nums[right]) {
-               right++;
-           }
-           cnt += (right - (mid + 1));
-       }
-       return cnt;
-   }
-   
-   private static int mergeSort(int[] nums, int low, int high) {
-       if (low >= high) return 0;
-       int mid = (low + high) / 2;
-       int cnt = mergeSort(nums, low, mid);
-       cnt += mergeSort(nums, mid + 1, high);
-       cnt += CountPairs(nums, low, mid, high);
-       merge(nums, low, mid, high);
-       return cnt;
-   }
+    public int reversePairs(int[] nums) {
+      int[] ans = {0};
+      mergeSort(nums , 0 , nums.length-1 , ans );
+        return ans[0] ;
+    }
+    public void mergeSort(int[] arr , int low , int high , int[] ans ){
+        if(low >= high ){
+            return ;
+        }
+        int mid = (low + high )/2;
+        mergeSort(arr , low , mid  , ans );
+        mergeSort(arr , mid+1 , high , ans);
+        merge(arr , low , mid , high , ans );
+
+    }
+    void merge(int[] arr , int low ,  int mid , int high , int[] ans ){
+        int left = low ;
+        int right = mid + 1; 
+        while(left <= mid  && right <= high  ){
+            if((long) arr[left] > 2L * arr[right]){
+                ans[0]+= mid-left + 1 ;
+                right++;
+            }
+            else{
+                left++;
+            }
+        }
+        int lefty = low ;
+        int righty = mid + 1;
+        List<Integer> temp = new ArrayList<>();
+
+        while(lefty <= mid  && righty <= high  ){
+            if(arr[lefty] >= arr[righty]){
+            temp.add(arr[righty++]);
+            }else{
+            temp.add(arr[lefty++]);
+            }
+        }
+        while(lefty <= mid){
+            temp.add(arr[lefty++]);
+        }
+        while(righty <= high){
+            temp.add(arr[righty++]);
+        }
+
+        for(int i = low ; i <= high ; i++){
+            arr[i] = temp.get(i-low);
+        }
+    }
 }
