64653400f5949b80d175a9d4636d90c3c7f427ba
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-11-05 00:05:32 +0530

    Time: 222 ms (40.34%) | Memory: 47.2 MB (5.58%) - LeetSync

--- 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.java
+++ 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/*import java.util.*;
 class Solution {
     public int maxSumSubmatrix(int[][] matrix, int k) {
         int m = matrix.length, n = matrix[0].length;
@@ -34,6 +34,42 @@ class Solution {
                 max = Math.max(max, prefixSum - target);
             }
             set.add(prefixSum);
+        }
+        return max;
+    }
+} */
+
+class Solution {
+    public int maxSumSubmatrix(int[][] arr, int k) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+        int maxK = Integer.MIN_VALUE;
+        for(int i = 0; i < cols; i++){
+            int dp[] = new int[rows];
+            for(int j = i; j < cols; j++){
+                for(int l = 0; l < rows; l++){
+                    dp[l] += arr[l][j];
+                }
+                int currSum = maxSubArray(dp, k);
+                maxK = Math.max(maxK, currSum);                 
+                if(maxK == k)
+                    return k;
+            }
+        }
+        return maxK;
+    }
+    public int maxSubArray(int[] arr, int k) {
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+        TreeSet<Integer> set = new TreeSet<>();
+        set.add(0);
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+            Integer ceilValue = set.ceiling(currSum - k);
+            if(ceilValue != null) {
+                max = Math.max(max, currSum - ceilValue);
+            }
+            set.add(currSum);
         }
         return max;
     }
