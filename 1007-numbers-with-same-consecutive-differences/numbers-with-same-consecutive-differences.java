0eb568d2279708cdfa23ca044615a22f7fa2634c
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2025-11-12 09:51:08 +0530

    Time: 1 ms (99.76%) | Memory: 43 MB (34.28%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.java
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.java
@@ -1,17 +1,49 @@
+// class Solution {
+//        public int[] numsSameConsecDiff(int N, int K) {
+//         List<Integer> cur = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//         for (int i = 2; i <= N; ++i) {
+//             List<Integer> cur2 = new ArrayList<>();
+//             for (int x : cur) {
+//                 int y = x % 10;
+//                 if (y + K < 10)
+//                     cur2.add(x * 10 + y + K);
+//                 if (K > 0 && y - K >= 0)
+//                     cur2.add(x * 10 + y - K);
+//             }
+//             cur = cur2;
+//         }
+//         return cur.stream().mapToInt(j->j).toArray();
+//     }
+// }
+
+
+
 class Solution {
-       public int[] numsSameConsecDiff(int N, int K) {
-        List<Integer> cur = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        for (int i = 2; i <= N; ++i) {
-            List<Integer> cur2 = new ArrayList<>();
-            for (int x : cur) {
-                int y = x % 10;
-                if (y + K < 10)
-                    cur2.add(x * 10 + y + K);
-                if (K > 0 && y - K >= 0)
-                    cur2.add(x * 10 + y - K);
-            }
-            cur = cur2;
+    public int[] numsSameConsecDiff(int n, int k) {
+        for(int i=1;i<=9;i++){
+            helper(i,n-1,k);
         }
-        return cur.stream().mapToInt(j->j).toArray();
+        int ans[] = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            ans[i]=list.get(i);
+        }
+
+        return ans;
+        
+    }
+    List<Integer>list = new ArrayList<>();
+
+    public void helper(int curr, int digitLeft, int k){
+        int lastDig = curr%10;
+        if(digitLeft==0){
+            list.add(curr);
+            return;
+        }
+        if(lastDig+k<=9){
+            helper(curr*10+lastDig+k, digitLeft-1, k);
+        }
+        if(k!=0 && lastDig-k>=0){ // lastDig+k == lastDig-k when k==0; to avoid this duplicacy we will check k!=0 before calling for lastDig-k
+            helper(curr*10+lastDig-k, digitLeft-1,k);
+        }
     }
 }
