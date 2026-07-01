edef37154228906d16594ce1ea50811483157591
Raghunandan <160055641+Raghunandan0@users.noreply.github.com>
2026-07-02 01:35:23 +0530

    Time: 1 ms (59.82%) | Memory: 48.6 MB (22.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -8,10 +8,10 @@ class Solution {
             left[i] = Math.max(left[i-1], height[i]);
         }
         right[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--){
+        for(int i=n-2; i>= 0; i--){
             right[i] = Math.max(right[i+1], height[i]);
         }
-        int ans = 0;
+        int ans= 0;
         for(int i=0; i<n; i++){
             ans += (Math.min(left[i],right[i])-height[i]);
         }
